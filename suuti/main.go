8a68c6b78d85df7384b665a8697c427828f16c25
sharknasuhorse <naka-ryo6459@nifty.com>
2017-05-05 02:53:29 +0900

    3-7

--- suuti/main.go
+++ suuti/main.go
@@ -5,6 +5,29 @@ import (
 )
 
 func main() {
+	fprint()
+	intprint()
+	hensu()
+}
+
+func fprint() {
 	fmt.Printf("10進数=%d 2進数=%b¥n", 17, 17)
 	fmt.Println()
+}
+
+func intprint() {
+	var x int
+	x = 10
+	x -= 20
+	fmt.Println(x)
+
+	y := 50
+	y = 20
+	println(y)
+}
+
+func hensu() {
+	o := 20
+	p := 90
+	println(o, p)
 }
